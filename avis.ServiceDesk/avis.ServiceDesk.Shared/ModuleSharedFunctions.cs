57e9048674c4bc269ce081361f3cbabc7c83923d
Sergei Samsonov <s.samsonov@avis-expert.ru>
2024-02-09 11:25:51 +0300

    Создан модуль ServiceDesk.

---
+++ avis.ServiceDesk/avis.ServiceDesk.Shared/ModuleSharedFunctions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace avis.ServiceDesk.Shared
+{
+  public class ModuleFunctions
+  {
+
+  }
+}
