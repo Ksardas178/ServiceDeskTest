cd0521469b968b1868fa46084fcea6eaa4f4e0fb
Sergei Samsonov <s.samsonov@avis-expert.ru>
2024-02-12 09:51:22 +0300

    Создан справочник "Типы обращений".

---
+++ avis.ServiceDesk/avis.ServiceDesk.Server/RequestType/RequestTypeHandlers.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using avis.ServiceDesk.RequestType;
+
+namespace avis.ServiceDesk
+{
+}
