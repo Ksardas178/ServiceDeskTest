58a580d0dbddff01448e6673be99c448100e20a5
Sergei Samsonov <s.samsonov@avis-expert.ru>
2024-02-12 09:58:43 +0300

    Создан справочник "Срочности".

---
+++ avis.ServiceDesk/avis.ServiceDesk.Server/Urgency/UrgencyServerFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using avis.ServiceDesk.Urgency;
+
+namespace avis.ServiceDesk.Server
+{
+  partial class UrgencyFunctions
+  {
+
+  }
+}
